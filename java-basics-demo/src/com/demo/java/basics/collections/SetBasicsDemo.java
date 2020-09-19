a676125276cbb912c9ae75518a0c156204e49faa
chandan-soud <chandan.k.soud@gmail.com>
2020-09-19 15:35:37 +0200

    Added demo for basic Set operations

--- java-basics-demo/src/com/demo/java/basics/collections/SetBasicsDemo.java
+++ java-basics-demo/src/com/demo/java/basics/collections/SetBasicsDemo.java
@@ -15,11 +15,50 @@ public class SetBasicsDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("This class is used for demonstrating basic set features");
+		System.out.println("This class is used for demonstrating basic Set features");
+		basicSetOperations();
 		basicBulkSetOperations();
 	}
 
+	/**
+	 * This method demonstrates the basic operations in Set
+	 * 
+	 */
+	public static void basicSetOperations() {
+
+		System.out.println("\n\nThis method demonstrates the basic operations in Set\n");
+
+		Set<Integer> s1 = new HashSet<>();
+		s1.add(1);
+		s1.add(3);
+		s1.add(4);
+		s1.add(2);
+
+		System.out.println("set1: " + s1);
+		System.out.println("size of set1: " + s1.size());
+
+		System.out.println("Adding a new element to set1: " + s1.add(10));
+		System.out.println("Adding an existing element to set1: " + s1.add(4));
+
+		System.out.println("Removing an existing element from set1: " + s1.remove(2));
+		System.out.println("Removing a non-existing element from set1: " + s1.remove(5));
+
+		System.out.println("Altered set1: " + s1);
+
+		System.out.println("set1 now contains 10: " + s1.contains(10));
+		System.out.println("set1 now doesn't contain 2: " + !s1.contains(2));
+
+		System.out.println("set1 is empty: " + s1.isEmpty());
+
+	}
+
+	/**
+	 * This method demonstrates the basic bulk operations in Set
+	 * 
+	 */
 	public static void basicBulkSetOperations() {
+
+		System.out.println("\n\nThis method demonstrates the basic bulk operations in Set\n");
 
 		// Create int arrays
 		int[] a1 = { 1, 2, 3, 4 };
@@ -41,14 +80,22 @@ public class SetBasicsDemo {
 		System.out.println("set1: " + s1);
 		System.out.println("set2: " + s2);
 
+		// Create a set with all elements from two sets
 		Set<Integer> union = new HashSet<>(s1);
 		union.addAll(s2);
 		System.out.println("union: " + union);
 
+		// Create a set with common elements from two sets
 		Set<Integer> intersection = new HashSet<>(s1);
 		intersection.retainAll(s2);
 		System.out.println("intersection: " + intersection);
 
+		// Check if set1 is a subset of union
+		System.out.println("set1 is a subset of union : " + union.containsAll(s1));
+		System.out.println("set1 is a superset of intersection : " + s1.containsAll(intersection));
+		System.out.println("set2 is a subset of set1 : " + s1.containsAll(s2));
+
+		// Create a set with elements present only in one of two sets
 		Set<Integer> setAsymmetricDiff1 = new HashSet<>(s1);
 		setAsymmetricDiff1.removeAll(s2);
 		System.out.println("Set Asymmetric Difference (set1 - set2): " + setAsymmetricDiff1);
@@ -57,6 +104,7 @@ public class SetBasicsDemo {
 		setAsymmetricDiff2.removeAll(s1);
 		System.out.println("Set Asymmetric Difference (set2 - set1): " + setAsymmetricDiff2);
 
+		// Create a set with uncommon elements from two sets
 		Set<Integer> setSymmetricDiff = new HashSet<>(union);
 		setSymmetricDiff.removeAll(intersection);
 		System.out.println("Set Symmetric Difference: " + setSymmetricDiff);
